957137297265ee9c1399e190548861c5d8a0fe5e
ehzhilehzil <ehzilehzil@outlook.com>
2025-08-10 18:44:58 +0900

    complete proto type

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ use tao::{
     dpi::*, event::*, event_loop::*, window::*
 };
 use wry::*;
+use regex::*;
+
+#[derive(Debug, Clone)]
+enum MyEvent {
+    JsMessage(String),
+    OtherEvent,
+}
 
 fn main() {
-    let event_loop = EventLoop::new();
+    let event_loop = EventLoopBuilder::<MyEvent>::with_user_event().build();
+    let proxy = event_loop.create_proxy();
     let window = WindowBuilder::new()
         .with_background_color((255, 255, 255, 0))
         .with_title("심플 브라우저")
@@ -21,6 +29,10 @@ fn main() {
         .build_as_child(&window)
         .expect("메인 웹뷰 생성 실패");
     let sub_view = WebViewBuilder::new()
+        .with_ipc_handler(move |req| {
+            let msg = req.body().to_string();
+            proxy.send_event(MyEvent::JsMessage(msg)).ok();
+        })
         .with_bounds(Rect {
             position: Position::Physical(PhysicalPosition { x: 0, y: 0 }),
             size: Size::Physical(PhysicalSize { width: window.inner_size().width, height: 120 }),
@@ -49,11 +61,17 @@ fn main() {
                     position: Position::Physical(PhysicalPosition { x: 0, y: 0 }),
                     size: Size::Physical(PhysicalSize { width: width, height: 120 }),
                 }).unwrap();
-                
             },
             Event::RedrawRequested(_window_id) => {
                 // println!("wow!");
             }
+            Event::UserEvent(MyEvent::JsMessage(msg)) => {
+                let re = Regex::new(r"^(https?://)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(/.*)?$").unwrap();
+                if re.is_match(&msg) {
+                    let url = if msg.starts_with("http") { msg } else { format!("{}{}", "https://", msg) };
+                    main_view.load_url(&url).expect("웹 주소 형식이 잘못되었습니다.");
+                }
+            },
             _ => (),
         }
     });
